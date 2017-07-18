229825abe9a06b6d277dabd0b6a4a8f4391a5c38
odinokun <odinokun@gmail.com>
2017-07-18 16:45:01 +0300

    главная страница сделана полностью с адаптивом

--- src/js/modules/svg4everybody.js
+++ src/js/modules/svg4everybody.js
@@ -59,6 +59,20 @@ module.exports = function() {
     $('.video').addClass('play');
   });
 
+  
+  $('#burger label').on('click', function() {
+    $('#menu').toggleClass('active');
+    $('body').toggleClass('no-scroll');
+  });
+  
+  $('.menu__item').on('click', function() {
+    $('.submenu-list').removeClass('active');
+    $(this).children('.submenu-list').addClass('active');
+  });
+
+
+
+
   // end   SVG for IE
 
 };
