9e3ad8ea7fb81e2cdf4f8ccb4f55a23baccbb185
odinokun <odinokun@gmail.com>
2017-07-21 19:22:46 +0300

    100%

--- src/js/prog.js
+++ src/js/prog.js
@@ -37,3 +37,21 @@ function pageWidget(pages) {
 
 //====== Begin Programmer code ======
 
+// begin popup open
+$('.popup-open').on('click', function() {
+  $('.popup, .popup__layer').fadeIn();
+  return false;
+});
+// end   popup open
+
+// begin popup close
+$('.popup__layer, .popup__close').on('click', function() {
+  $('.popup, .popup__layer').fadeOut();
+});
+// end   popup close
+
+// begin popup scroll
+// $(window).on('scroll', function() {
+//   $('.popup').css({'top': $(window).scrollTop() + 50});
+// }).scroll();
+// end   popup scroll
